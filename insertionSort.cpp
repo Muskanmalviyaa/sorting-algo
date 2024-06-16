f8dad4b64af13282f606bba9eccc13d35b00c3be
Muskan malviya <muskanmalvi609@gmail.com>
2024-06-16 18:53:44 +0530

    oky

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,14 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 void insertionSort(int arr[], int n){
-    for(int i=1; i<n-1; i++){
-        int current = arr[i];
-        int j = i-1;
-        while(arr[j]>current && j>=0){
-            arr[j+1] = arr[j];
-            j--;
-        }
-        arr[j+1] = current;
+    for(int i=0; i<=n-1; i++){
+        // int current = arr[i];
+        int j = i;
+        while(arr[j-1]>arr[j] && j>0){
+        int temp = arr[j-1];
+        arr[j-1] = arr[j];
+        arr[j] = temp;
+        j--;
+      }
     }
 }
 int main(){
@@ -24,6 +25,6 @@ int main(){
     }
     return 0;
 }
-// 
+// avg/worst case TC = O(N^2) || best = O(N)
 // step-1: take the elements and
 // step-2: place it in it's current position
